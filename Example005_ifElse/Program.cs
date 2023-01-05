f7416d7be81b19bbdb2762f0e02b191279390c79
Aleksey Povalishin <povalishinalex@icloud.com>
2023-01-05 13:27:31 +0300

    add Example005_ifElse

---
+++ Example005_ifElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите Ваше имя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "полина")
+{
+    Console.WriteLine("Шаббат Шалом, Полина!");
+}
+else
+{
+    Console.Write("Эх, тогда иди в жопу, ");
+    Console.WriteLine(username);
+}
