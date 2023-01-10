7ef9e6f389753fa2432b5f5b3315ef694b4bb1ae
Aleksey Povalishin <povalishinalex@icloud.com>
2023-01-10 13:24:14 +0300

    add Example010_MethodArray

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] array = { 21, 32, 34, 45, 89, 65, 67, 87, 89 };
+int n = array.Length;
+int find = 89;
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+
+}
